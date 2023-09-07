dbcc0dd02c96cd11d4cf3156034fe3cba710a321
Richard <rahayn@yahoo.com>
2023-09-07 16:25:20 -0400

    Adding a feature allowing to switch between a list view or a chart view

--- MyNumbers/MyNumbers/Views/SalesGraphView.swift
+++ MyNumbers/MyNumbers/Views/SalesGraphView.swift
@@ -4,9 +4,18 @@
 //
 //  Created by richard Haynes on 7/19/23.
 //
+/*
+ https://sarunw.com/posts/swiftui-picker-enum/
+ https://sarunw.com/posts/swiftui-form-picker-styles/
+ */
 
 import SwiftUI
 import Charts
+enum GraphMode: String, CaseIterable {
+    case ListMode = "List"
+    case ChartMode = "Chart"
+    case Both = "Both"
+}
 struct SalesGraphView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
@@ -14,32 +23,74 @@ struct SalesGraphView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Sale.saleDate, ascending: true)],
         animation: .default)
     private var sales: FetchedResults<Sale>
+    @State private var mode : GraphMode = GraphMode.Both
     
     var body: some View {
         ZStack {
-            
             VStack {
                 Image("money")
                     .resizable()
                     .scaledToFit()
                 Text("Sales Over Time")
-                Chart {
-                    ForEach(sales) { sale in
-                        LineMark(
-                            x: .value("Month", sale.saleDate!, unit: .weekOfMonth),
-                            y: .value("Sales", sale.saleAmount)
-                        ).foregroundStyle(Color.green.gradient)
-                        
+                Picker("Mode", selection: $mode){
+                    ForEach(GraphMode.allCases, id: \.self){
+                        Text($0.rawValue)
                     }
-                }.frame(height: 180)
+                }.pickerStyle(.segmented)
+                switch self.mode {
+                case .ChartMode :
+                    ChartView(sales: self.sales)
+                case .ListMode :
+                    ListView(sales: self.sales)
+                case .Both :
+                    ChartView(sales: self.sales)
+                    ListView(sales: self.sales)
+                }
+                
             }.padding()
         }
             
     }
+    
 }
 
 struct SalesGraphView_Previews: PreviewProvider {
     static var previews: some View {
         SalesGraphView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+fileprivate struct ChartView : View {
+    var sales : FetchedResults<Sale>
+    
+    init(sales : FetchedResults<Sale>) {
+        self.sales = sales
+    }
+    var body: some View {
+        Chart {
+            ForEach(sales) { sale in
+                LineMark(
+                    x: .value("Month", sale.saleDate!, unit: .weekOfMonth),
+                    y: .value("Sales", sale.saleAmount)
+                ).foregroundStyle(Color.green.gradient)
+                    
+                
+            }
+        }.frame(height: 180)
+    }
+}
+
+fileprivate struct ListView : View {
+    var sales : FetchedResults<Sale>
+    
+    init(sales : FetchedResults<Sale>) {
+        self.sales = sales
+    }
+    var body: some View {
+        List {
+            ForEach(sales) { sale in
+                Text("\(sale.saleAmount.formatted(.currency(code: "USD"))) Sale Made on \(sale.saleDate!)")
+            }
+        }
     }
 }
